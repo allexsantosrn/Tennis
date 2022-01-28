f2096be65ad9045c054e427ec7f905e297e3ed43
allexkid <allexsantosrn@gmail.com>
2022-01-28 16:03:56 -0300

    Criação do projeto.
    Criação do projeto inicial.

---
+++ tennis.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+var jogador1 = "Nadal"
+
+var jogador2 = "Federer"
+
+func player(nome string, turn chan int) {
+
+	teste := <-turn
+
+	fmt.Println("Teste:", teste)
+
+	
+}
+
+func main() {
+
+	turn := make(chan int)
+
+	go player(jogador1, turn)
+	go player(jogador2, turn)
+
+	turn <- 1
+
+	time.Sleep(1e9)
+}
