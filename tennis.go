929acf90476bdf1e333d27e4c21d3c17f1a23c56
allexkid <allexsantosrn@gmail.com>
2022-01-28 22:06:58 -0300

    Primeira implementação funcional.
    Problema em execução, mas com pequenos bugs.

--- tennis.go
+++ tennis.go
@@ -6,9 +6,20 @@ import (
 	"time"
 )
 
+// Representa o número de pontos máximos que identificará o vencedor da partida.
+var pontosFixos = 5
+
+// Jogador 1
 var jogador1 = "Nadal"
 
+// Representa o número de pontos do jogador 1.
+var pontosj1 = 0
+
+// Jogador 2
 var jogador2 = "Federer"
+
+// Representa o número de pontos do jogador 2.
+var pontosj2 = 0
 
 func jogador(nome string, turn chan int) {
 
@@ -23,6 +34,7 @@ func jogador(nome string, turn chan int) {
 			return
 		}
 
+		// Gera um número aleatório que auxilia no processo de não acerto da bola.
 		num := gerarNumeroAleatorio()
 
 		if num%7 == 0 {
@@ -30,8 +42,33 @@ func jogador(nome string, turn chan int) {
 			fmt.Printf("Jogador %s não acertou a bola (Jogada %d).", nome, posse)
 			fmt.Print("\n")
 
-			close(turn)
-			return
+			// Incrementa pontuação para o jogador 2, caso o jogador 1 não tenha acertado a bola.
+			if nome == jogador1 {
+
+				pontosj2++
+				fmt.Printf("Ponto para %s, totalizando %d ponto(s).", jogador2, pontosj2)
+				fmt.Print("\n \n")
+				fmt.Printf("Placar atual - %s: %d x %d: %s", jogador1, pontosj1, pontosj2, jogador2)
+				fmt.Print("\n \n")
+
+				// Incrementa pontuação para o jogador 2, caso o jogador 1 não tenha acertado a bola.
+			} else {
+
+				pontosj1++
+				fmt.Printf("Ponto para %s, totalizando %d ponto(s).", jogador1, pontosj1)
+				fmt.Print("\n \n")
+				fmt.Printf("Placar atual - %s: %d x %d: %s", jogador1, pontosj1, pontosj2, jogador2)
+				fmt.Print("\n \n")
+			}
+
+			// Encerra a partida caso alguns dos jogadores atinga o valor limite.
+			if pontosj1 == pontosFixos || pontosj2 == pontosFixos {
+				fmt.Printf("Jogador %s perdeu a partida.", nome)
+				fmt.Print("\n")
+				close(turn)
+				return
+			}
+
 		}
 
 		fmt.Printf("Jogador %s acertou a bola (Jogada %d).", nome, posse)
@@ -44,17 +81,20 @@ func jogador(nome string, turn chan int) {
 
 func main() {
 
+	rand.Seed(time.Now().UnixNano())
+
 	turn := make(chan int)
 
 	go jogador(jogador1, turn)
 	go jogador(jogador2, turn)
 
+	// Iniciando na jogada um.
 	turn <- 1
 
 	time.Sleep(1e9)
 }
 
-// Gera um número aleatório até o valor de 50.
+// Gera um número inteiro aleatório até o limite de 50.
 func gerarNumeroAleatorio() int {
 
 	numeroAleatorio := rand.Intn(50)
